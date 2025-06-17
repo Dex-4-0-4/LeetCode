3b60fc7b2c3420994d2f793d8e06a39b21ae0857
Debojyoti Dev <debojyotidev17@gmail.com>
2025-06-17 09:54:08 +0530

    Time: 0 ms (100%), Space: 21.8 MB (70.36%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,24 +3,19 @@ public:
     int missingNumber(vector<int>& nums) {
 
         int len = nums.size();
-        int ans=len;
-
+        int ans = len;
         for(int i=0;i<len;i++){
-            
             while(nums[i]!=i){
                 if(nums[i]>len-1)
                 break;
                 swap(nums[i],nums[nums[i]]);
             }
-
         }
         for(int i=0;i<len;i++){
-            if(nums[i]!=i){
-                ans = i;
-                break;
-            }
+            if(nums[i]!=i)
+            ans = i;
         }
-        
         return ans;
+        
     }
 };
