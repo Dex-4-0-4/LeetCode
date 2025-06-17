88c1c3ff08482764bdf89646620f319326c15d9b
Debojyoti Dev <debojyotidev17@gmail.com>
2025-06-17 10:28:44 +0530

    Time: 5 ms (23.12%), Space: 8.6 MB (43.02%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-
+        long s=0;
         if(x<0)
         return false;
         else{
-        long s=0;
-        int bk = x;
-        while(bk!=0){
-            int rem = bk%10;
-            bk/=10;
-            s = s*10+rem;
+            int bk=x;
+            while(bk!=0){
+                int rem = bk%10;
+                bk /=10;
+                s = s*10 + rem;
+            }
+            if(s==x)
+            return true;
+            else
+            return false;
+            
         }
-        if(s==x)
-        return true;
-        else
-        return false;
-        }
+
         
     }
 };
